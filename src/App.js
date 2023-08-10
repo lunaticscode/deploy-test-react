ac11f03ea94b993f20e2d022a3120cd847063008
lunaticscode <lunatics384@gmail.com>
2023-08-10 16:27:01 +0900

    nav 바 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 
 function App() {
-  return <Outlet />;
+  const navigate = useNavigate();
+  return (
+    <>
+      <head>
+        <nav onClick={() => navigate("/")}>home</nav>
+        <nav onClick={() => navigate("/shop")}>shop</nav>
+        <nav onClick={() => navigate("/profile")}>profile</nav>
+      </head>
+      <Outlet />
+    </>
+  );
 }
 
 export default App;
